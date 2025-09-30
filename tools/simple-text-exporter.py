2f7775ead2988315ddca44d6358028e698e6b72d
kazuya-awano <kazuya-awano@webfreak.jp>
2025-09-30 14:46:03 +0900

    initial development

---
+++ tools/simple-text-exporter.py
@@ -0,0 +1,25 @@
+from collections.abc import Generator
+from typing import Any
+
+from dify_plugin import Tool
+from dify_plugin.entities.tool import ToolInvokeMessage
+
+from tools.utils.mimetype import MimeType
+
+class SimpleTextExporterTool(Tool):
+    def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
+        text = tool_parameters.get("text")
+        output_filename = tool_parameters.get("output_filename")
+        result_file_bytes = text.encode("utf-8")
+
+        yield self.create_blob_message(
+            blob=result_file_bytes,
+            meta={
+                "mime_type": MimeType.guess_mime_type(output_filename),
+                "output_filename": output_filename,
+            }
+        )
+        yield self.create_text_message(
+            text=output_filename
+        )
+        return
